5ef48a2a57c4116e541cf9b77be25d2f056c9b5c
TheRexous <144456224+TheRexous@users.noreply.github.com>
2024-04-11 14:17:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const circle = document.querySelector('.circle')
+let count = 0
+
+function toggleColor() {
+    circle.classList.toggle('white-circle')
+    count++
+    console.log(count)
+    if (count > 9) {
+        circle.removeEventListener('click', toggleColor)
+    }
+}
+
+circle.addEventListener('click', toggleColor)
+
+let height = 0
+
+function moveCircle() {
+    height += 100
+    circle.style.top = height + 'px'
+    if (height > 300) {
+        circle.removeEventListener('click', moveCircle)
+    }
+}
+
+circle.addEventListener('click', moveCircle)
